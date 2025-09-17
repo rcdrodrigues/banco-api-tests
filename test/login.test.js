26b44ec7313abb61c6cdb76427cf13e3df2be420
Ricardo Rodrigues <originalrcd@hotmail.com>
2025-09-17 20:21:59 -0300

    sem console.log

--- test/login.test.js
+++ test/login.test.js
@@ -11,8 +11,8 @@ describe ('Login', () => {
                     'username': 'julio.lima',
                     'senha': '123456'
                     });
-            console.log(resposta.status) 
-            console.log(resposta.body)       
+            //console.log(resposta.status) 
+            //console.log(resposta.body)       
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
                     
